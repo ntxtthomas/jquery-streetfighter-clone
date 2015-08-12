b1017e42e9d9911d7e13618a3c89678997c9e971
ntxtthomas <apm.tthomas@gmail.com>
2015-08-12 14:12:00 -0500

    update near end

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
-/*console.log('DEBUG 1');
+var pressed=false
 $(document).ready(function(){
-    console.log('DEBUG doc ready');
-    $('.ryu').mouseenter(function() {
-        console.log('DEBUG %o',$('.ryu-still'),1+1);
-        $('.ryu-still').hide();
-        //$('.ryu-ready').show();
-    });
-});*/
+    doIntro();
 
-$(document).ready(function(){
     $('.ryu').mouseenter(function(){
         $('.ryu-still').hide();
         $('.ryu-ready').show();
@@ -25,6 +18,7 @@ $(document).ready(function(){
             {'left': '1020px'},
             500,
             function() {
+                $(this).stop();
                 $(this).hide();
                 $(this).css('left', '520px');
         })
@@ -35,15 +29,21 @@ $(document).ready(function(){
         })
 });
 $(document).keydown(function(e){
+        if(pressed)
+            return;
         if (e.keyCode == 88) {
+            pressed=true;
         playCool();
-            $('.ryu-still','.ryu-ready').hide();
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
             $('.ryu-cool').show();
         }
     })
     .keyup(function(e){
         if (e.keyCode == 88) {
-            $('#Cool')[0].pause();
+            pressed=false;
+            $('#Cool-sound')[0].pause();
+            $('#Cool-sound')[0].load();
             $('.ryu-cool').hide();
             $('.ryu-still').show();
         }
@@ -58,3 +58,22 @@ function playCool() {
         $('#Cool-sound')[0].load();
         $('#Cool-sound')[0].play();
     };
+function doIntro() {
+        $('#theme-song')[0].volume = 0.25;
+        $('#theme-song')[0].load();
+        $('#theme-song')[0].play();
+        $('.sf-logo').fadeIn(3500, function() {
+            $(this).fadeOut(1000, function() {
+                $('.brought-by').fadeIn(1500, function() {
+                    $(this).fadeOut(1000, function() {
+                        $('.jquery-logo').fadeIn(1500, function() {
+                            $(this).fadeOut(1500, function() {
+                                $('.how-to').fadeIn(1000);
+    
+            });
+          })
+        })
+      })
+    })
+  })
+}
